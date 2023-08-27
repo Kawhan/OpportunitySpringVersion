ec8501cc7831203705a35e44b4d527a1f2b7dcc3
Kawhan <kawhanlaurindo@outlook.com>
2023-08-27 15:56:42 -0300

    commit files

--- opportunityHub/src/main/java/br/ufpb/dcx/oppfyhub/opportunityhub/controler/JobController.java
+++ opportunityHub/src/main/java/br/ufpb/dcx/oppfyhub/opportunityhub/controler/JobController.java
@@ -4,6 +4,7 @@ import br.ufpb.dcx.oppfyhub.opportunityhub.dto.JobTitleRequestDTO;
 import br.ufpb.dcx.oppfyhub.opportunityhub.dto.JobRequestDTO;
 import br.ufpb.dcx.oppfyhub.opportunityhub.dto.JobResponseDTO;
 import br.ufpb.dcx.oppfyhub.opportunityhub.service.JobService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -42,7 +44,7 @@ public class JobController {
 
     @GetMapping("{titleJob}")
     @ResponseStatus(code = HttpStatus.OK)
-    public List<JobResponseDTO> getJobByTitleJob(@RequestParam String titleJob) {
+    public List<JobResponseDTO> getJobByTitleJob(@RequestParam @Valid String titleJob) {
         return jobService.getJobByTitleJob(titleJob);
     }
 
@@ -50,20 +52,20 @@ public class JobController {
 
     @PostMapping
     @ResponseStatus(code=HttpStatus.CREATED)
-    public JobResponseDTO createJob(@RequestBody JobRequestDTO jobRequestDTO) {
+    public JobResponseDTO createJob(@RequestBody @Valid JobRequestDTO jobRequestDTO) {
         return jobService.createJob(jobRequestDTO);
     }
 
-    @PostMapping("{id}/change")
+    @PutMapping("{id}/change")
     @ResponseStatus(code=HttpStatus.OK)
-    public JobResponseDTO changeInfoJob(@PathVariable long id, @RequestBody JobRequestDTO jobRequestDTO) {
+    public JobResponseDTO changeInfoJob(@PathVariable long id, @RequestBody @Valid JobRequestDTO jobRequestDTO) {
         return jobService.changeInfoJob(id, jobRequestDTO);
     }
 
     // Patches
     @PatchMapping("{id}/title")
     @ResponseStatus(code=HttpStatus.OK)
-    public JobResponseDTO changeTitleJob(@PathVariable long id, @RequestBody JobTitleRequestDTO jobTitleRequestDTO) {
+    public JobResponseDTO changeTitleJob(@PathVariable long id, @RequestBody @Valid JobTitleRequestDTO jobTitleRequestDTO) {
         return jobService.changeTitleJob(id, jobTitleRequestDTO);
     }
 
