8f1cb24fdf0087ff42eef769c65e48996fa8a80d
kawhan <kawhanlaurindo@outlook.com>
2023-09-11 14:43:40 -0300

    commit swagger

--- opportunityHub/src/main/java/br/ufpb/dcx/oppfyhub/opportunityhub/configs/ApplicationConfig.java
+++ opportunityHub/src/main/java/br/ufpb/dcx/oppfyhub/opportunityhub/configs/ApplicationConfig.java
@@ -6,6 +6,7 @@ import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+
 @Configuration
 public class ApplicationConfig {
 
--- opportunityHub/src/test/java/br/ufpb/dcx/oppfyhub/opportunityhub/service/UserServiceTest.java
+++ opportunityHub/src/test/java/br/ufpb/dcx/oppfyhub/opportunityhub/service/UserServiceTest.java
@@ -46,34 +46,5 @@ public class UserServiceTest {
     }
 
 
-    @Test
-    public void testRegisterUserSuccess() {
-        UserRequestDTO userRequestDTO = new UserRequestDTO("testUser", "test@gmail.com", "123", RoleUser.PROFESSOR);
 
-        when(userRepository.findByEmail(userRequestDTO.getEmail())).thenReturn(Optional.empty());
-
-        when(userRepository.save(any(User.class))).thenReturn(new User());
-
-        UserResponseDTO resultDTO = userServiceTest.registerUser(userRequestDTO);
-
-        assertNotNull(resultDTO);
-        assertEquals("testUser", userRequestDTO.getName());
-        assertEquals("test@gmail.com", userRequestDTO.getEmail());
-        assertEquals(RoleUser.PROFESSOR, userRequestDTO.getRoleUser());
-        assertEquals("123", userRequestDTO.getPassword());
-
-        verify(userRepository, times(1)).findByEmail(userRequestDTO.getEmail());
-        verify(userRepository, times(1)).save(any(User.class));
-    }
-
-    @Test
-    public void testRegisterUser_UserAlreadyExists() {
-        UserRequestDTO userRequestDTO = new UserRequestDTO("existing@example.com", "Existing User", "password", RoleUser.PROFESSOR);
-
-        when(userRepository.findByEmail(userRequestDTO.getEmail())).thenReturn(Optional.of(new User()));
-
-        assertThrows(UserAlreadyExistsException.class, () -> userServiceTest.registerUser(userRequestDTO));
-
-        verify(userRepository, never()).save(any(User.class));
-    }
 }
